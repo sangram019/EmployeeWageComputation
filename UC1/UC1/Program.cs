1c9323a21c73988021f9a03069711979ffde0729
Sangram <sangram15.behera@gmail.com>
2022-04-22 03:16:55 +0530

    Employee Attendance

---
+++ UC1/UC1/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Employeewage computation!");
+
+Console.WriteLine("Welcome to Employee Wage computation program!");
+
+Random Attendance = new Random();
+int ispresent = Attendance.Next(0, 2);
+Console.WriteLine(ispresent);
+if (ispresent == 1)
+    Console.WriteLine("Employee is present!");
+else
+    Console.WriteLine("Employee is Absent");
