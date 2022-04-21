b691173b018e9ef3f4122368bd9dac317af18f59
Sangram <sangram15.behera@gmail.com>
2022-04-22 03:56:38 +0530

    Use Case 5 Added By Sangram

---
+++ UC1/UC1/UC5.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeWageComputation
+{
+
+    class UC5
+    {
+        public void month()
+        {
+            Console.WriteLine("Welcome to Employee Wage computation program!");
+
+            int empwageperhrs = 20;
+            int emphrs = 0;
+            int workingdays = 20;
+            Random CheckempAttandance = new Random();
+            int ispresent = CheckempAttandance.Next(0, 4);
+            Console.WriteLine(ispresent);
+
+            if (ispresent == 1)
+            {
+                Console.WriteLine("Employee is present!");
+                Console.WriteLine("Employee is Full Time");
+                emphrs = 8;
+            }
+            else if (ispresent == 2)
+            {
+                Console.WriteLine("Employee is Present!");
+                Console.WriteLine("Employee is Part Time");
+                emphrs = 4;
+            }
+
+            else
+            {
+                Console.WriteLine("Employee is Absent!");
+                emphrs = 0;
+            }
+
+
+            switch (ispresent)
+            {
+                case 1:
+                    Console.WriteLine("Employee is present!");
+                    Console.WriteLine("Employee is Full Time");
+                    emphrs = 8;
+                    break;
+                case 2:
+                    Console.WriteLine("Employee is present!");
+                    Console.WriteLine("Employee is Part Time");
+                    emphrs = 4;
+                    break;
+                case 0:
+                    Console.WriteLine("Employee is Absent!");
+
+                    emphrs = 0;
+                    break;
+                default:
+                    Console.WriteLine("error");
+                    break;
+
+            }
+            int Salary = emphrs * empwageperhrs;
+            int monthsalary = Salary * workingdays;
+            Console.WriteLine("Employee Salary = " + Salary);
+            Console.WriteLine("Finalmonthsalary = " + monthsalary);
+        }
+    }
+}
